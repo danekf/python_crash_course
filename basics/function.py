5ce3f7b17943babd2ed2db8fe79e8647831f8e4c
Danek <danek.fill@outlook.com>
2022-11-25 11:25:39 -0500

    add reminder for keyword argument for functions

--- basics/function.py
+++ basics/function.py
@@ -1,8 +1,19 @@
-user = input('What is your name? ')
 
-def greet_user():
+def greet_user(user):
   print(f'Hello {user}')
   print('Lets get coding')
+  
 
+name = input('What is your name? ')
+greet_user(name)
 
-greet_user()
+#Keyword arguments, positiong doesnt matter
+def greet_user_key(first, last):
+  print(f'Hello {first} {last}')
+  print('Lets get coding')
+  
+
+f_name = input('What is your first name? ')
+l_name = input('What is your last name? ')
+
+greet_user_key(last = l_name, first = f_name)
