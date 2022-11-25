f7a38f40b17fca3a8cc3c6dac4ffcc58fc7c2f13
Danek <danek.fill@outlook.com>
2022-11-25 16:11:15 -0500

    update admin display of products

--- django_website/products/admin.py
+++ django_website/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+#import products into admin capability
+from .models import Product
 
-# Register your models here.
+
+#create new class based off the admin model in admin
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ('name', 'price', 'stock')
+
+
+
+#give ability for admin to create product, and use information in the Product admin class to show the things in the table.
+admin.site.register(Product, ProductAdmin)
--- django_website/products/models.py
+++ django_website/products/models.py
@@ -6,14 +6,14 @@ class Product(models.Model):
   #create attributes using the Model attributes that django understands
   name = models.CharField(max_length=255)
   price = models.FloatField()
-  stock = models.IntegerField
+  stock = models.IntegerField()
   image_url = models.CharField(max_length=2083)
 
-  
+
 class Offer(models.Model):
   code = models.CharField(max_length=10)
   description = models.CharField(max_length=255)
-  discount = models.IntegerField
+  discount = models.IntegerField()
   
 
 #
