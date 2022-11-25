3a2089578be24332c9f1ddde3e486730dc77cffc
Danek <danek.fill@outlook.com>
2022-11-25 14:39:10 -0500

    add simple automation example using openpyxl

--- automation_example/app.py
+++ automation_example/app.py
@@ -3,14 +3,47 @@
 #import open excel workbook, and name it xl for easier reference
 import openpyxl as xl
 
-#load the file
-wb = xl.load_workbook('transactions.xlsx')
-#load the sheet to work on
-sheet = wb['Sheet1']
+#add package to add bar charts
+from openpyxl.chart import BarChart, Reference
 
-#load the cell we want to work in
-cell = sheet['a1']
-#alternatively
-cell = sheet.cell(1,1)
 
-print (cell.value)
+def process_workbook(filename):
+  #load the file
+  wb = xl.load_workbook(filename)
+  #load the sheet to work on
+  sheet = wb['Sheet1']
+
+  #loop through all rows in the sheet, starting at the one AFTER the titles
+  for row in range(2, sheet.max_row + 1):
+    #we want to affect the PRICE and update it, so select row3 and go to column 3 and set that as the target cell
+    cell = sheet.cell(row, 3)
+    print(cell.value)
+
+    #adjust for 90% of price
+    corrected_price = cell.value*0.9
+
+    #add new column with new price rather than overwrite it in column 4
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+
+  #add bar chart to workbook after the loop using the new data created in col 4 for all rows
+  values = Reference(sheet, 
+            min_row=2, 
+            max_row=sheet.max_row,
+            min_col=4,
+            max_col=4)
+
+  chart = BarChart()
+  chart.add_data(values)
+
+  #now that the chart is defined, add it using the created chart, at positiong e2
+  sheet.add_chart(chart, 'e2')
+
+  #save workbook
+  wb.save(filename)
+
+
+#and now call it
+file = input('Enter Filename to update, including extension: ')
+process_workbook(file)
